30e44e18cd360857d7a5131c7342b1765029e9c3
Hectornauta <hemilianop13@ca.frre.utn.edu.ar>
2020-09-25 04:08:42 -0300

    Segundo commit
    Carga y muestra el dataset en forma de gráfico

--- src/UI/archivo.py
+++ src/UI/archivo.py
@@ -8,6 +8,9 @@ class Archivo():
         self.clases = []
         self.numfilas = 0
         self.numcolumnas = 0
+        self.x = []
+        self.y = []
+        self.clases = []
     def abrir(self):
         with open(self.ruta, encoding='utf8') as csvfile:
 	        self.datos = list(csv.reader(csvfile,delimiter=';'))
@@ -15,5 +18,9 @@ class Archivo():
         self.datos.remove(self.datos[0])
         self.numfilas = len(self.datos)
         self.numcolumnas = len(self.datos[0])
+        for dato in self.datos:
+            self.x.append(float(dato[0]))
+            self.y.append(float(dato[1]))
+            self.clases.append(dato[2])
     def datos(self):
         return self.datos
